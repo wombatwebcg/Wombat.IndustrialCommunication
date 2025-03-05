450ab7d7996e308636e3ac53e2ffa2614a946eaa
wombatwebcg <wombatwebcg@gmail.com>
2025-03-05 11:40:48 +0800

    推送

---
+++ Wombat.IndustrialCommunication/ObservableArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+using System.Collections.Generic;
+
+namespace Wombat.IndustrialCommunication
+{
+
+
+    public class ObservableArray<T> where T : IEquatable<T>
+    {
+        private Memory<T> memory;
+        private T[] lastSnapshot;
+        private ConcurrentQueue<(int index, T oldValue, T newValue)> changeQueue = new ConcurrentQueue<(int index, T oldValue, T newValue)>();
+        private CancellationTokenSource cts = new CancellationTokenSource();
+
+        public event Action<int, T, T> OnElementChanged;
+
+        public ObservableArray(int length)
+        {
+            memory = new Memory<T>(new T[length]);
+            lastSnapshot = new T[length];
+
+            // 启动异步通知任务
+            Task.Run(ProcessChanges);
+        }
+
+        public Span<T> AsSpan() => memory.Span;
+
+        public void Set(int index, T value)
+        {
+            if (!memory.Span[index].Equals(value))
+            {
+                T oldValue = memory.Span[index];
+                memory.Span[index] = value;
+
+                // 把变化放入队列（非阻塞）
+                changeQueue.Enqueue((index, oldValue, value));
+            }
+        }
+
+        public void CheckForChanges()
+        {
+            var currentSpan = memory.Span;
+            for (int i = 0; i < currentSpan.Length; i++)
+            {
+                if (!currentSpan[i].Equals(lastSnapshot[i]))
+                {
+                    changeQueue.Enqueue((i, lastSnapshot[i], currentSpan[i]));
+                    lastSnapshot[i] = currentSpan[i]; // 更新快照
+                }
+            }
+        }
+
+        private async Task ProcessChanges()
+        {
+            while (!cts.Token.IsCancellationRequested)
+            {
+                while (changeQueue.TryDequeue(out var change))
+                {
+                    OnElementChanged?.Invoke(change.index, change.oldValue, change.newValue);
+                }
+                await Task.Delay(10); // 小延迟，批量处理
+            }
+        }
+
+        public void StopWatching()
+        {
+            cts.Cancel();
+        }
+    }
+}
---
+++ Wombat.IndustrialCommunicationTestProject/Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wombat.IndustrialCommunication;
+using Xunit;
+
+namespace Wombat.IndustrialCommunicationTestProject
+{
+    public class Tests
+    {
+
+        [Fact]
+
+        public void ObservableArrayTest()
+        {
+            var array = new ObservableArray<int>(100000)
+            {
+                //BatchInterval = TimeSpan.FromMilliseconds(10),
+                //EnableBatchNotification = true
+            };
+
+            array.OnElementChanged += (index, oldValue, newValue) =>
+            {
+                Debug.WriteLine($"Index {index} changed from {oldValue} to {newValue},{DateTime.Now.ToString("HH:mm:ss ffff")}");
+
+            };
+
+            //array.OnBatchChanged += batchChanges =>
+            //{
+            //    Debug.WriteLine($"Batch Update: {batchChanges.Count} elements changed,{DateTime.Now.ToString("HH:mm:ss ffff")}");
+            //};
+
+            //Parallel.For(0, 100000, i =>
+            //{
+            //    array.Set(i, i * 10);
+            //    array.Set(i, i * 20);
+            //});
+
+            for (int i = 0; i < 100000; i++)
+            {
+                array.Set(i, i * 10);
+
+            }
+            //Console.WriteLine($"Latest value at index 5: {array.GetLatest(5)}");
+
+            Thread.Sleep(70000);
+            array.StopWatching();
+        }
+    }
+}
